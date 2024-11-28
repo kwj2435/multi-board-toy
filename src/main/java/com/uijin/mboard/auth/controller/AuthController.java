3c74dd97a28ea989dd44cb44d1422849f83a3440
kimuijin <dmlwls2435@naver.com>
2024-11-28 19:59:31 +0900

    feature.카카오 로그인 구현 진행

--- src/main/java/com/uijin/mboard/auth/controller/AuthController.java
+++ src/main/java/com/uijin/mboard/auth/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.uijin.mboard.auth.controller;
 
+import com.uijin.mboard.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/oauth")
 public class AuthController {
+
+  private final AuthService authService;
 
   @GetMapping("/kakao/callback")
   public void kakaoCallback() {
--- src/main/java/com/uijin/mboard/member/MemberController.java
+++ src/main/java/com/uijin/mboard/member/MemberController.java
@@ -1,6 +1,8 @@
 package com.uijin.mboard.member;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 public class MemberController {
 
-  @GetMapping("/login")
-  public String login() {
-    return "login";
+  @Value("${kakao.oauth.client-id}")
+  private String clientId;
+
+  @Value("${kakao.oauth.redirect-uri}")
+  private String redirectUri;
+
+  @GetMapping("/login/kakao")
+  public String login(Model model) {
+    String kakaoAuthDomain = "https://kauth.kakao.com/oauth/authorize";
+    String responseType = "code";
+
+    String kakaoLoginUrl = kakaoAuthDomain + "?" +
+        "response_type=" + responseType +
+        "&client_id=" + clientId +
+        "&redirect_uri=" + redirectUri;
+
+    model.addAttribute("kakaoLoginUri", kakaoLoginUrl);
+
+    return "kakaoLogin";
   }
 }
